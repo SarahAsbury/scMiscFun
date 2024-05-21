7dc8930a927dc87c91b0832c8a0f0581362fefe6
SarahAsbury <83042411+SarahAsbury@users.noreply.github.com>
2024-05-21 16:42:37 -0400

    add mito/ribo/hemo plot

---
+++ R/plot_mrh.R
@@ -0,0 +1,64 @@
+#' plot_sample_qc
+#'
+#' plot standard quality control metrics of mitochondrial, ribosomal, and hemoglobin gene counts. designed to work with run_sc_qc pipeline only.
+#'
+#' @param sce sce object
+#' @param plot_by character vector; variables to plot by
+#' @param mit_cutoff numeric value; a horizontal line will be added at this threshold.
+#' @return list of plots
+#' @examples
+#' plot_mrh(sce, plot_by = c("sample", "annot"), mit_cutoff = 5)
+#' @import dplyr
+#' @import purrr
+#' @import SingleCellExperiment
+#' @import tidySingleCellExperiment
+#' @import scater
+#' @import cowplot
+#' @export
+
+plot_mrh <- function(sce,
+                     plot_by,
+                     mit_cutoff)
+  # Purpose: plot mitochondrial, ribosomal and heme qc parameters
+{
+  map(c(plot_by),
+
+      ~plot_grid(
+        # mito
+        plotColData(sce, x = "sum", y="subsets_Mito_percent", colour_by=.x) +
+          theme(legend.position = "none") +
+          geom_hline(yintercept = mit_cutoff, linetype="dashed", color = "black", size = 0.4),
+
+        plotColData(sce, x = .x, y="subsets_Mito_percent", colour_by=.x) +
+          theme(legend.position = "none") +
+          geom_hline(yintercept = mit_cutoff, linetype="dashed", color = "black", size = 0.4) +
+          theme(axis.text.x = element_blank()),
+
+        #ribo
+        plotColData(sce, x = "sum", y="subsets_Ribo_percent", colour_by=.x) +
+          theme(legend.position = "none"),
+
+        plotColData(sce, x = .x, y="subsets_Ribo_percent", colour_by=.x) +
+          theme(legend.position = "none") +
+          theme(axis.text.x = element_blank()),
+
+        # rbc
+        plotColData(sce, x = "sum", y="subsets_Hemo_percent", colour_by=.x) +
+          theme(legend.position = "none"),
+
+        plotColData(sce, x = .x, y="subsets_Hemo_percent", colour_by=.x) +
+          theme(legend.position = "none") +
+          theme(axis.text.x = element_blank()),
+
+        ncol = 2
+      ) %>%
+        # plot just legend
+        plot_grid(.,
+                  plotColData(sce, x = .x, y="subsets_Mito_percent", colour_by=.x) %>% cowplot::get_legend(),
+                  rel_widths = c(5,1),
+                  ncol = 2)
+
+
+  ) %>% setNames(plot_by)
+}
+
