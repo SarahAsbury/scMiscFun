702b6f50f74cc37c7617519b54de72f48d9005e0
SarahAsbury <83042411+SarahAsbury@users.noreply.github.com>
2024-05-21 18:20:15 -0400

    bug fix plot_mrh

--- R/plot_mrh.R
+++ R/plot_mrh.R
@@ -27,7 +27,7 @@ plot_mrh <- function(sce,
 
       ~{
         plots <- list()
-        if("m" %in% include){
+        if(str_detect(regex("m", ignore_case = T))){
           plots <- c(plots,
                      list(
                        plotColData(sce, x = "sum", y="subsets_Mito_percent", colour_by=.x) +
@@ -42,7 +42,7 @@ plot_mrh <- function(sce,
           )
         }
 
-        if("r" %in% include){
+        if(str_detect(regex("r", ignore_case = T))){
           plots <- c(plots,
                      list(
                        plotColData(sce, x = "sum", y="subsets_Ribo_percent", colour_by=.x) +
@@ -56,7 +56,7 @@ plot_mrh <- function(sce,
 
         }
 
-        if("h" %in% include){
+        if(str_detect(regex("h", ignore_case = T))){
           plots <- c(plot,
                      list(plotColData(sce, x = "sum", y="subsets_Hemo_percent", colour_by=.x) +
                             theme(legend.position = "none"),
