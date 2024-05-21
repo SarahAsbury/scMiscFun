1909e2557b04ca6ce27e75395c127b804cd70f18
SarahAsbury <83042411+SarahAsbury@users.noreply.github.com>
2024-05-21 17:14:22 -0400

    update plot_mrh

--- R/plot_mrh.R
+++ R/plot_mrh.R
@@ -5,9 +5,10 @@
 #' @param sce sce object
 #' @param plot_by character vector; variables to plot by
 #' @param mit_cutoff numeric value; a horizontal line will be added at this threshold.
+#' @param include character vector containing m, r, and/or h. If m is in vector, mitochondrial genes will be plotted. If r, ribosomal genes. If h, hemoglobin genes.
 #' @return list of plots
 #' @examples
-#' plot_mrh(sce, plot_by = c("sample", "annot"), mit_cutoff = 5)
+#' plot_mrh(sce, plot_by = c("sample", "annot"), mit_cutoff = 5, include = "mrh")
 #' @import dplyr
 #' @import purrr
 #' @import SingleCellExperiment
@@ -18,46 +19,67 @@
 
 plot_mrh <- function(sce,
                      plot_by,
-                     mit_cutoff)
+                     mit_cutoff,
+                     include = "mrh"
+                     )
   {
   map(c(plot_by),
 
-      ~plot_grid(
-        # mito
-        plotColData(sce, x = "sum", y="subsets_Mito_percent", colour_by=.x) +
-          theme(legend.position = "none") +
-          geom_hline(yintercept = mit_cutoff, linetype="dashed", color = "black", size = 0.4),
+      ~{
+        plots <- list()
+        if("m" %in% include){
+          plots <- c(plots,
+                     list(
+                       plotColData(sce, x = "sum", y="subsets_Mito_percent", colour_by=.x) +
+                         theme(legend.position = "none") +
+                         geom_hline(yintercept = mit_cutoff, linetype="dashed", color = "black", size = 0.4),
 
-        plotColData(sce, x = .x, y="subsets_Mito_percent", colour_by=.x) +
-          theme(legend.position = "none") +
-          geom_hline(yintercept = mit_cutoff, linetype="dashed", color = "black", size = 0.4) +
-          theme(axis.text.x = element_blank()),
+                       plotColData(sce, x = .x, y="subsets_Mito_percent", colour_by=.x) +
+                         theme(legend.position = "none") +
+                         geom_hline(yintercept = mit_cutoff, linetype="dashed", color = "black", size = 0.4) +
+                         theme(axis.text.x = element_blank())
+                     )
+          )
+        }
 
-        #ribo
-        plotColData(sce, x = "sum", y="subsets_Ribo_percent", colour_by=.x) +
-          theme(legend.position = "none"),
+        if("r" %in% include){
+          plots <- c(plots,
+                     list(
+                       plotColData(sce, x = "sum", y="subsets_Ribo_percent", colour_by=.x) +
+                         theme(legend.position = "none"),
 
-        plotColData(sce, x = .x, y="subsets_Ribo_percent", colour_by=.x) +
-          theme(legend.position = "none") +
-          theme(axis.text.x = element_blank()),
+                       plotColData(sce, x = .x, y="subsets_Ribo_percent", colour_by=.x) +
+                         theme(legend.position = "none") +
+                         theme(axis.text.x = element_blank())
+                     )
+          )
 
-        # rbc
-        plotColData(sce, x = "sum", y="subsets_Hemo_percent", colour_by=.x) +
-          theme(legend.position = "none"),
+        }
 
-        plotColData(sce, x = .x, y="subsets_Hemo_percent", colour_by=.x) +
-          theme(legend.position = "none") +
-          theme(axis.text.x = element_blank()),
+        if("h" %in% include){
+          plots <- c(plot,
+                     list(plotColData(sce, x = "sum", y="subsets_Hemo_percent", colour_by=.x) +
+                            theme(legend.position = "none"),
 
-        ncol = 2
-      ) %>%
-        # plot just legend
+                          plotColData(sce, x = .x, y="subsets_Hemo_percent", colour_by=.x) +
+                            theme(legend.position = "none") +
+                            theme(axis.text.x = element_blank())
+                     )
+          )
+        }
+
+
+
+
+        plot_grid(plots,
+                  ncol = 2
+        ) %>%
+          # plot just legend
         plot_grid(.,
                   plotColData(sce, x = .x, y="subsets_Mito_percent", colour_by=.x) %>% cowplot::get_legend(),
                   rel_widths = c(5,1),
                   ncol = 2)
-
-
-  ) %>% setNames(plot_by)
+      }) %>%
+            setNames(plot_by)
 }
 
